c096036a832f15bd3f13cefd33e82338ed4731b3
Revati Nimbalkar <Revati.Nimbalkar@bmwtechworks.in>
2026-01-21 17:17:41 +0530

    new files added

---
+++ bti_structure.py
@@ -0,0 +1,65 @@
+from abc import ABC , abstractmethod
+
+class BTI:
+    def __init__(self, name , email, office, cluster, unit):
+        self.name = name
+        self.email = email
+        self.office = office
+        self.cluster = cluster
+        self.unit = unit
+
+class Org(ABC):
+    def __init__(self , name , count , budget):
+        self.name = name
+        self.count = count
+        self.budget = budget
+        self.parent = list["node"] = None
+        self.child = list["none"] = []
+
+    def add(self, ):
+
+    def remove(self, ):
+
+    def kpi(self,):
+
+    def set_kpi(self,):
+
+class Employee()
+        
+
+class Pune_hub(Hub):
+    
+
+
+
+
+
+
+
+
+
+
+
+class Pune_Hub():
+    def __init__(self , name , email, office, cluster, unit):
+        super().__init__(name , email, office, cluster, unit)
+        self.cluster = cluster
+
+class Digital_Car():
+    def __init__(self , name , email , cluster , unit):
+        super().__init__(name , email , cluster)
+        self.unit = unit
+
+class Digital_Company():
+    def __init__(self , name , email , cluster , unit):
+        super().__init__(name, email, cluster)
+        self.unit = unit
+
+class Digital_Product():
+    def __init__(self, name , email ,cluster , unit):
+        super().__init__(name, email, cluster, unit)
+        self.unit = unit
+
+class 
+
+
